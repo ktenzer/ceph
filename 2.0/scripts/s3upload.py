feea2cdc8fd041a1c138cf90a80558073520c735
root <root@ceph1.lab.com>
2017-02-01 22:31:06 +0100

    first commit

---
+++ 2.0/scripts/s3upload.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+
+import boto
+import boto.s3.connection
+from boto.s3.key import Key
+
+access_key = 'PYVPOGO2ODDQU24NXPXZ'
+secret_key = 'pM1QULv2YgAEbvzFr9zHRwdQwpQiT9uJ8hG6JUZK'
+rgw_hostname = 'ceph1'
+rgw_port = 8080
+local_testfile = '/tmp/testfile'
+bucketname = 'mybucket'
+
+
+conn = boto.connect_s3(
+	aws_access_key_id = access_key,
+	aws_secret_access_key = secret_key,
+	host = rgw_hostname,
+	port = rgw_port,
+	is_secure=False,
+	calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+	)
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '#'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+    if iteration == total: 
+        print()
+
+def percent_cb(complete, total):
+    printProgressBar(complete, total)
+
+bucket = conn.create_bucket('mybucket')
+for bucket in conn.get_all_buckets():
+	print "{name}\t{created}".format( name = bucket.name, created = bucket.creation_date,)
+
+bucket = conn.get_bucket(bucketname) 
+
+k = Key(bucket)
+k.key = 'my test file'
+k.set_contents_from_filename(local_testfile, cb=percent_cb, num_cb=20)
